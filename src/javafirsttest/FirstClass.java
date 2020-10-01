febc00eb31c2f1fe8f838c4ab8ef6897792f7960
clara <clara@DESKTOP-CLARUCA>
2020-10-01 21:07:57 +0200

    mensaje Hola

---
+++ src/javafirsttest/FirstClass.java
@@ -0,0 +1,32 @@
+
+
+/**
+ *
+ * @author clara
+ */
+public class FirstClass {
+    
+    
+    
+    public static void main(String[] args){
+        
+        String nombre = "Clara";
+        int numero1, numero2, total;
+        numero1 = 5;
+        numero2 = 6;
+        
+        total = numero1 + numero2;
+        
+        if(numero1 < numero2){
+            System.out.println(total);
+        }
+        else{
+            System.out.println("Error");
+        }
+        
+        
+        
+    }
+    
+    
+}
