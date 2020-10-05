8d4641c3281bb876a83e455f5601a04856420326
Claruca <cmartialberti@gmail.com>
2020-10-05 17:05:45 +0200

    clase 1

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,7 +3,28 @@
  * @author clara
  */
 public class FirstClass {
-    public static void main(String[] args){       
+    public static void main(String[] args){     
+        
+        String helloWorld = "Hello World";
+        System.out.println(helloWorld);     
+        
+        
+        final double PI = 3.14;
+        char grupo = 'B';
+        int contador = 34;   
+        float resultado = 34.3f;
+        double d = 34.3;
+        
+        float euros, dolares;
+        euros = 427;
+        dolares = euros * 1.17f;
+  
+        System.out.println("Los " + euros + "euros, son " + dolares + " dolares.");       
+        
+        
+        
+        
+        /*
       
         String nombres = "Clara";
         int numero1, numero2, total;
@@ -19,17 +40,25 @@ public class FirstClass {
         else{
             System.out.println("Error ");
         }
-        
+       
+        int cadena[] = new int[4];
+        cadena[0] = 2;
+        cadena[1] = 4;
+        cadena[2] = 6;
+                
+        System.out.println(cadena[0]);
+                
         for(int i=1; i<10; i++){
           System.out.println(i);
+          
         }
         
         while(numero1<10){
             numero1++;  
             System.out.println(numero1);
     }
-        
-        
+       
+    */
     }
     
     
