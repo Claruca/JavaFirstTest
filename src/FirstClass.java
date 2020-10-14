d275536155628d6baf5bac2a9665b2a769084bc0
clara <clara@DESKTOP-CLARUCA>
2020-10-14 19:46:21 +0200

    horoscopo

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,106 +1,127 @@
+
 /**
  *
  * @author clara
  */
 public class FirstClass {
-    public static void main(String[] args){     
+
+    public static void main(String[] args) {
         
-        String helloWorld = "Hello World";
-        System.out.println(helloWorld);     
+        int year = 1932;
+        int resto = year%12;
+        String horoscopo = "";
+       /* 
+        switch(year){
+            case 1924: case 1936: case 1948: System.out.println("Rata");
+            break;
+            case 1925: case 1937: case 1949: System.out.println("Buey");
+            break;
+            case 1926: case 1938: case 1950: System.out.println("Tigre");
+            break;
+            case 1927: case 1939: case 1951: System.out.println("Conejo");
+            break;
+            case 1928: case 1940: case 1952: System.out.println("Dragón");
+            break;
+        
+        }*/
+        
+        switch(resto){
+            case 0: horoscopo = "Mono"; break;
+            case 1: horoscopo = "Gallo"; break;
+            case 2: horoscopo = "Perro"; break;
+            case 3: horoscopo = "Cerdo"; break;
+            case 4: horoscopo = "Rata"; break;
+            case 5: horoscopo = "Buey"; break;
+            case 6: horoscopo = "Tigre"; break;
+            case 7: horoscopo = "Conejo"; break;
+            case 8: horoscopo = "Dragon"; break;
+            case 9: horoscopo = "Serpiente"; break;
+            case 10: horoscopo = "Caballo"; break;
+            case 11: horoscopo = "Cabra"; break;
+        
+        }
+        
+        System.out.println("Tu horóscopo chino, según el año de nacimiento es: " + horoscopo);
         
         
+    
+        
+    }
+
+     /*   String helloWorld = "Hello World";
+        System.out.println(helloWorld);
+
         final double PI = 3.14;
         char grupo = 'B';
-        int contador = 34;   
+        int contador = 34;
         float resultado = 34.3f;
         double d = 34.3;
-        
+
         float euros, dolares;
         euros = 427;
         dolares = euros * 1.17f;
-  
-        System.out.println("Los " + euros + "euros, son " + dolares + " dolares.");       
-        
-        
-        
+                
+   
+
+
+        System.out.println("Los " + euros + "euros, son " + dolares + " dolares.");
+
         boolean V = true;
         boolean F = false;
-        
-        System.out.println(V&&F);
-        System.out.println(F&&V);
-        System.out.println(V&&V);
-        System.out.println(F&&F);
-        System.out.println(V||F);
-        System.out.println(V||F);
-        System.out.println(V||F);
-        System.out.println(V||F);
-        
-        
+
+        System.out.println(V && F);
+        System.out.println(F && V);
+        System.out.println(V && V);
+        System.out.println(F && F);
+        System.out.println(V || F);
+        System.out.println(V || F);
+        System.out.println(V || F);
+        System.out.println(V || F);
+
         int numero = 10;
+        int numero1 = 6;
         int numero2 = 3;
-        
-        if(numero>0){
+
+        if (numero > 0) {
             System.out.println(numero + "es positivo");
-        }
+        }      
         
-        if(numero<1&&numero>-1){
+        
+        
+        if (numero < 1 && numero > -1) {
             System.out.println(numero);
         }
-        
-        
-        if(numero%2 == 0){
+
+        if (numero % 2 == 0) {
             System.out.println(numero + " múltiplo de 2");
-        }
-        else{
+        } else {
             System.out.println(numero + " no es múltiplo de 2");
         }
-        
-        
-        if(numero%numero2 ==0){
+
+        if (numero % numero2 == 0) {
             System.out.println(numero + " múltiplo");
         }
         System.out.println(numero + " no es múltiplo");
-            
-        }
-        
-        
-        
-        /*
-      
-        String nombres = "Clara";
-        int numero1, numero2, total;
-        numero1 = 6;
-        numero2 = 9;
-        
-        total = numero1 + numero2;
-        
-        if(numero1 < numero2){
-            System.out.println(total + " " + nombres);
-            
-        }
-        else{
-            System.out.println("Error ");
-        }
-       
+
         int cadena[] = new int[4];
         cadena[0] = 2;
         cadena[1] = 4;
         cadena[2] = 6;
-                
+
         System.out.println(cadena[0]);
-                
-        for(int i=1; i<10; i++){
-          System.out.println(i);
-          
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+
         }
-        
-        while(numero1<10){
-            numero1++;  
+
+        while (numero1 < 10) {
+            numero1++;
             System.out.println(numero1);
+
+        }
+*/
+     
+     
     }
-       
-    */
-    }
-    
-    
-}
+
